698e87d14c71133f695568024bc4fef717f1e4f9
Marvin Menzel <93315675+marvintrvl@users.noreply.github.com>
2024-06-11 15:40:01 +0200

    new look

--- photos/admin.py
+++ photos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Photo
+from .models import Category, Photo, Subcategory
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Photo)
+admin.site.register(Subcategory)
